5737f790b2ebeeb8a59c3cd524407ea623f14751
Daniel Avzaradel <daniela@cgen.com>
2022-05-24 01:07:18 +0300

    refactoring routes

---
+++ backend/controllers/goalController.js
@@ -0,0 +1,38 @@
+// @desc    Get Goals
+// @route   GET /api/goals
+// @acces   Private
+
+const getGoals = (req, res) => {
+  res.status(200).json({ message: 'Success! Get Goals!' });
+};
+
+// @desc    Set Goals
+// @route   POST /api/goals
+// @acces   Private
+
+const setGoal = (req, res) => {
+  res.status(200).json({ message: 'Success! Set Goal!' });
+};
+
+// @desc    Update Goals
+// @route   PUT /api/goals/:id
+// @acces   Private
+
+const updateGoal = (req, res) => {
+  res.status(200).json({ message: `Updated goal ${req.params.id}` });
+};
+
+// @desc    Delete Goal
+// @route   DELETE /api/goals/:id
+// @acces   Private
+
+const deleteGoal = (req, res) => {
+  res.status(200).json({ message: `Deleted goal ${req.params.id}` });
+};
+
+module.exports = {
+  getGoals,
+  setGoal,
+  updateGoal,
+  deleteGoal,
+};
--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -1,20 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const { getGoals, setGoal, updateGoal, deleteGoal } = require('../controllers/goalController');
 
-router.get('/', (req, res) => {
-  res.status(200).json({ message: 'Success! Get Goals!' });
-});
-
-router.post('/', (req, res) => {
-  res.status(200).json({ message: 'Success! Set Goal!' });
-});
-
-router.put('/:id', (req, res) => {
-  res.status(200).json({ message: `Updated goal ${req.params.id}` });
-});
-
-router.delete('/:id', (req, res) => {
-  res.status(200).json({ message: `Deleted goal ${req.params.id}` });
-});
+router.route('/').get(getGoals).post(setGoal);
+router.route('/:id').put(updateGoal).delete(deleteGoal);
 
 module.exports = router;
