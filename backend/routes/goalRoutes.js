eb3239abc6f97290602308a24c05be711c51b9d2
Daniel Avzaradel <daniela@cgen.com>
2022-05-24 00:24:08 +0300

    creating routes for CRUD

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -5,4 +5,16 @@ router.get('/', (req, res) => {
   res.status(200).json({ message: 'Success! Get Goals!' });
 });
 
+router.post('/', (req, res) => {
+  res.status(200).json({ message: 'Success! Set Goal!' });
+});
+
+router.put('/:id', (req, res) => {
+  res.status(200).json({ message: `Updated goal ${req.params.id}` });
+});
+
+router.delete('/:id', (req, res) => {
+  res.status(200).json({ message: `Deleted goal ${req.params.id}` });
+});
+
 module.exports = router;
